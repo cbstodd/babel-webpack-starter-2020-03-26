35f87ce32cb2f570481572df148b29e4ae18f042
Colper <cbstodd@gmail.com>
2020-03-27 00:26:18 -0400

    Installed RxJS and added support for HTMLCSS in Webpack via loaders

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,33 @@
 import './constants';
 import people from './data';
+import { Observable } from 'rxjs/Observable';
+import { of, map } from 'rxjs/operators';
 
-const listUsers = _users => {
-  if (_users) {
-    return _users.forEach(usr => usr);
-  } else {
-    console.error(`cbsError: Could not get users`);
-  }
-};
+const allPeople = [];
+
+Observable.create(subscriber => {
+     const users = Object.keys(people).map(key => people[key]);
+     subscriber.next(users);
+  subscriber.complete();
+}).subscribe(user => {
+  user.map(usr => this.allPeople = usr);
+});
+
+// for (const _key in users) {
+//   if (users.hasOwnProperty(_key)) {
+//     const element = users[_key];
+//   }
+// }
+
+console.table(await allPeople);
+
+// const listUsers = _users => {
+//   if (_users) {
+//     return _users.forEach(usr => usr);
+//   } else {
+//     console.error(`cbsError: Could not get users`);
+//   }
+// };
 
 // const allUsers = listUsers(people);
 
